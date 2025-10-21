310f1d718d42535f406f0b5800a22083f32ae23a
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-10-21 14:58:54 -0600

    S2 - Pro tip: Asignaciones con operadores

--- 02-control-cycles-and-structures/04-boolean-logic.js
+++ 02-control-cycles-and-structures/04-boolean-logic.js
@@ -33,6 +33,19 @@ console.log(true || false);
 console.log( returnTrue() || returnFalse());
 console.log( returnFalse() || returnTrue());
 
+console.warn('Asignaciones');
 
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
 
+const a1 = true && 'Hola mundo';
+const a2 = 'Hola' && 'Mundo' && soyFalso;
+const a3 = soyFalso || 'Ya no soy falso';
+const a4 = soyFalso || soyNull || soyUndefined || 'Ya no soy falso de nuevo' || true;
+
+console.log({a1});
+console.log({a2});
+console.log({a3});
+console.log({a4});
 
