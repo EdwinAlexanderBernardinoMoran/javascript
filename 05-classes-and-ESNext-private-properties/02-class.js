4bfff661866e5fc1ec00502b5d49c49ac8498bf5
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-10-26 20:46:22 -0600

    S5 - Sets y Gets

--- 05-classes-and-ESNext-private-properties/02-class.js
+++ 05-classes-and-ESNext-private-properties/02-class.js
@@ -2,12 +2,21 @@ class Person{
 
     name;
     age;
+    foot;
 
     constructor(name, age){
 
         if (!name) throw Error("Name is required");
         this.name = name;
         this.age = age;
+    }
+
+    set setFoodFavorite( food ) {
+        this.foot = food.toUpperCase();
+    }
+
+    get getFoodFavorite() {
+        return `${this.name}'s favorite food is ${this.foot}`;
     }
 
     whoAmI(){
@@ -17,7 +26,15 @@ class Person{
 
 const spiderMan = new Person("Peter Parker", 25);
 const iroMan = new Person("Tony Stark", 48);
+
+spiderMan.setFoodFavorite = "Pizza";
+
+// Esta propiedad no existe en la clase Person y se puede agregar ( no es algo bueno)
+spiderMan.nemesis = "Green Goblin";
+
 console.log(spiderMan);
+console.log(spiderMan.getFoodFavorite);
+
 console.log(iroMan);
 
 spiderMan.whoAmI();
