dc8eab7cbed17f4dfda2d5837c91215aa333d038
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-10-26 20:11:58 -0600

    S5 - Metodos en las clases

--- 05-classes-and-ESNext-private-properties/02-class.js
+++ 05-classes-and-ESNext-private-properties/02-class.js
@@ -9,9 +9,16 @@ class Person{
         this.name = name;
         this.age = age;
     }
+
+    whoAmI(){
+        console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
+    }
 }
 
 const spiderMan = new Person("Peter Parker", 25);
 const iroMan = new Person("Tony Stark", 48);
 console.log(spiderMan);
 console.log(iroMan);
+
+spiderMan.whoAmI();
+iroMan.whoAmI();
