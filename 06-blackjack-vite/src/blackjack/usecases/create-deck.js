378ac9b6f1ec1e412badc91b292951dca2e5327a
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-11-05 19:24:19 -0600

    S6 - Mejorar la documentacion JSDoc Comments

--- 06-blackjack-vite/src/blackjack/usecases/create-deck.js
+++ 06-blackjack-vite/src/blackjack/usecases/create-deck.js
@@ -1,7 +1,17 @@
 import _ from 'underscore';
 
-// This function creates a new deck
+
+/**
+ * Create a new deck of cards
+ * @param {Array<String>} cardType 
+ * @param {Array<String>} specialTypes 
+ * @returns {Array<String>} deck
+ */
 export const createDeck = (cardType, specialTypes) => {
+
+    if (!cardType || !cardType.length === 0) throw new Error('Card types are required to create a deck');
+    if (!specialTypes || specialTypes.length === 0) throw new Error('Special card types are required to create a deck');
+
     let deck = [];
 
     for (let i = 2; i <= 10; i++){
