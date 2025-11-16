29dd98d49560e3bd15b63b9089221cbc20d78335
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-11-15 21:42:51 -0600

    S8 - Callback Hell

--- 08-advanced/src/concepts/02-callbacks.js
+++ 08-advanced/src/concepts/02-callbacks.js
@@ -5,14 +5,23 @@ import { heroes } from "../data/hero"
  * @param {HTMLDivElement} element 
  */
 export const callbacksComponents = (element) => {
-    const id = '5d86371f97c29d020f1e1f6ds'
+    const id = '5d86371f97c29d020f1e1f6d'
+    const idTwo = '5d86371fd55e2e2a30fe1ccb'
     findHero( id, (error, hero) => {
         // element.innerHTML = hero?.name || 'No se encontro el heroe'
         if (error) {
             element.innerHTML = error;
             return;
         }
-        element.innerHTML = hero.name;
+
+        findHero( idTwo, (error, hero2) => {
+            if (error) {
+                element.innerHTML = error;
+                return;
+            }
+
+            element.innerHTML = `${ hero.name } / ${ hero2.name }`;
+        })
     })
 }
 
