bc04f93c918a3a74b34a81756b464a7157f19b74
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-11-17 14:56:28 -0600

    S8 - Promise Hell

--- 08-advanced/src/concepts/03-promises.js
+++ 08-advanced/src/concepts/03-promises.js
@@ -9,14 +9,31 @@ export const promisesComponents = (element) => {
         element.innerHTML = hero.name;
     }
 
+    const renderTwoHeroes = (hero, heroTwo) => {
+        element.innerHTML = `${ hero.name } / ${ heroTwo.name }`;
+    }
+
     const renderError = (error) => {
         element.innerHTML = `<span style="color:red">${ error }</span>`;
     }
 
     const id = '5d86371f97c29d020f1e1f6d'
+    const idTwo = '5d86371fd55e2e2a30fe1ccb1'
+
     // findHero(id).then(hero => renderHero(hero))
+    // findHero(id)
+    //     .then(renderHero)
+    //     .catch(renderError);
+
+    // Promise Hell
     findHero(id)
-        .then(renderHero)
+        .then(hero => {
+            findHero(idTwo)
+                .then(heroTwo => {
+                    renderTwoHeroes(hero, heroTwo)
+                })
+                .catch(renderError)
+        })
         .catch(renderError);
 }
 
