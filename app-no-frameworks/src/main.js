e1301c5f3504416d61dde590a95f072240842acd
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-11-23 15:01:02 -0600

    S9 - Carga de usuarios

--- app-no-frameworks/src/main.js
+++ app-no-frameworks/src/main.js
@@ -1,7 +1,8 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+
+import { UsersApp } from './users/app.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -13,12 +14,10 @@ document.querySelector('#app').innerHTML = `
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+const element = document.querySelector('.card')
+
+UsersApp(element)
