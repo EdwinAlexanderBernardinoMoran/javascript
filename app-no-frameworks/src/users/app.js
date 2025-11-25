15efb024b4a0e33597bce0022c4e93dd9c4a3b29
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-11-25 15:13:01 -0600

    S9 - Crear modal manualmente

--- app-no-frameworks/src/users/app.js
+++ app-no-frameworks/src/users/app.js
@@ -1,5 +1,6 @@
 import { renderAddButton } from "./presentations/render-add-button/render-add-button";
 import { renderButtons } from "./presentations/render-buttons/render-buttons";
+import { renderModal } from "./presentations/render-modal/render-modal";
 import { renderTable } from "./presentations/render-table/render-table";
 import usersStore from "./store/users.store"
 
@@ -15,4 +16,5 @@ export const UsersApp = async (element) => {
     renderTable(element);
     renderButtons(element);
     renderAddButton(element);
+    renderModal(element)
 }
